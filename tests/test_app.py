af3c17fd25885fa83a5a8a1e25ac5d23cb5f1660
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-05-10 21:36:44 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from wrapper_geocode_reverse.app import app
+
+
+def test_root_and_return_running():
+    client = TestClient(app)
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'status': 'running'}
