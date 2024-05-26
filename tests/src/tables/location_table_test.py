e4099232f368db9a54072fcd6f68f607213ddad9
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-05-25 21:36:38 -0300

    test(location_table_test): added test for model of the table location

---
+++ tests/src/tables/location_table_test.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select
+
+from wrapper_geocode_reverse.src.tables.location_table import LocationTable
+
+
+def test_create_location(session):
+    latitude, longitude = 1, 1
+    point = f'POINT({latitude} {longitude})'
+    new_location = LocationTable(
+        address='address',
+        latitude=1.0,
+        longitude=1.0,
+        city='city',
+        state='state',
+        country='country',
+        postal_code='postal_code',
+        latitude_longitude=point,  # type: ignore
+    )
+
+    session.add(new_location)
+    session.commit()
+
+    location = session.scalar(
+        select(LocationTable).where(LocationTable.latitude_longitude == point)
+    )
+
+    assert location.latitude == latitude
+    assert location.longitude == longitude
