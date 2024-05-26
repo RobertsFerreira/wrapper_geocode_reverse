82e910a2e183e58dc73502ef27bdb92a78d59a4b
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-05-25 21:36:09 -0300

    test(conftest): added config for sessions test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import os
+
 import pytest
 from fastapi.testclient import TestClient
+from geoalchemy2 import load_spatialite
 from sqlalchemy import create_engine
+from sqlalchemy.event import listen
 from sqlalchemy.orm import Session
 
 from wrapper_geocode_reverse.app import app
@@ -15,6 +19,13 @@ def client():
 @pytest.fixture()
 def session():
     engine = create_engine('sqlite:///:memory:')
+
+    base_path = "E:\\ProjetosPY\\wrapper_geocode_reverse"
+    path = f'{base_path}\\plugins\\spatialite\\mod_spatialite.dll'
+
+    os.environ['SPATIALITE_LIBRARY_PATH'] = path
+
+    listen(engine, 'connect', load_spatialite)
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
