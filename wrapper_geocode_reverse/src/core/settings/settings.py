ab2019a65868be734c57b1ddd69b23f73bc45e53
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-07-03 23:47:57 -0300

    test: config mock tests

--- wrapper_geocode_reverse/src/core/settings/settings.py
+++ wrapper_geocode_reverse/src/core/settings/settings.py
@@ -11,3 +11,18 @@ class Settings(BaseSettings):
     SPATIALITE_DLL_URL: str
     OPEN_ROUTER_TOKEN: str
     OPEN_ROUTER_GEOCODE_REVERSE_URL: str
+
+    def __repr__(self) -> str:
+        return f"""
+                    Settings(
+                        DATABASE_URL={self.DATABASE_URL},
+                        SpatIALITE_DLL_URL={self.SPATIALITE_DLL_URL},
+                        OPEN_ROUTER_GEOCODE_REVERSE_URL={self.OPEN_ROUTER_GEOCODE_REVERSE_URL}
+                        )
+                    """
+
+
+def get_settings():
+    settings = Settings()  # type: ignore
+    print(settings)
+    return settings
