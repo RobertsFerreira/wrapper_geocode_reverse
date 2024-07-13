047e7823de7a4c645bf0b9205d507af662b6043b
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-07-13 09:56:40 -0300

    feat(location router): added location router

--- wrapper_geocode_reverse/app.py
+++ wrapper_geocode_reverse/app.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
-app = FastAPI()
+from wrapper_geocode_reverse.src.location.routers.location_router import (
+    location_router,
+)
+
+app = FastAPI(version='0.0.1', summary='Location')
+
+base_router = APIRouter(prefix='/v1')
+
+app.include_router(base_router)
+
+base_router.include_router(
+    location_router, prefix='/location', tags=['location']
+)
 
 
-@app.get('/')
+@base_router.get('/')
 def root():
-    return {'status': 'running'}
+    return {'status': 'on'}
---
+++ wrapper_geocode_reverse/src/location/routers/location_router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from pydantic_extra_types.coordinate import Coordinate, Latitude, Longitude
+
+from wrapper_geocode_reverse.src.location.service.location_service import (
+    LocationService,
+    get_service,
+)
+
+location_router = APIRouter()
+
+
+@location_router.get('/')
+async def get_location_by_lat_long(
+    lat: Latitude,
+    lon: Longitude,
+    service: LocationService = Depends(get_service),
+):
+    coordinate = Coordinate(lat, lon)
+    result = await service.reverse_geocode(coordinate=coordinate)
+    return result
